e13a1dc51efe634085d4ac7f9276b168087f4285
CopsiMan <raresfilip63@yahoo.com>
2021-10-11 12:48:32 +0300

    added more inserts for test

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,12 +1,18 @@
 from HashTable import HashTable
 
+
 def main():
     symbol_table = HashTable()
-    print(symbol_table.add("1"))
-    print(symbol_table.add("2"))
-    print(symbol_table.add("2"))
-    print(symbol_table.add("2"))
-    print(symbol_table.add("2"))
-    print(symbol_table.add("3"))
+    print(symbol_table.add(1))
+    print(symbol_table.add(2))
+    print(symbol_table.add(3))
+    print(symbol_table.add(4))
+    print(symbol_table.add(2))
+    print(symbol_table.add(3))
+    print(symbol_table.add("hello"))
+    print(symbol_table.add("hello1"))
+    print(symbol_table.add("hello2"))
+    print(symbol_table.add("hello3"))
+
 
 main()
