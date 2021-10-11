e93399c31e1eafb3b8910ba054312b24327c9a46
CopsiMan <raresfilip63@yahoo.com>
2021-10-11 12:40:04 +0300

    Lab 2

---
+++ Lab 2/main.py
@@ -0,0 +1,12 @@
+from HashTable import HashTable
+
+def main():
+    symbol_table = HashTable()
+    print(symbol_table.add("1"))
+    print(symbol_table.add("2"))
+    print(symbol_table.add("2"))
+    print(symbol_table.add("2"))
+    print(symbol_table.add("2"))
+    print(symbol_table.add("3"))
+
+main()
